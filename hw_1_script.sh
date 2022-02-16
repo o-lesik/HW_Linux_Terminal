7e19cfac6626639b1203b014c7fbd4446d220462
o-lesik <lesyamanina@yandex.ru>
2022-02-16 15:50:21 +0300

    "New files with HW"

---
+++ hw_1_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd group_27
+mkdir lesya_1 lesya_2 lesya_3
+cd lesya_1
+touch file_1.txt file_2.txt file_3.txt file_4.json file_5.json
+mkdir papka_1 papka_2 papka_3
+ls -la
+mv {file_1.txt,file_2.txt} papka_2
